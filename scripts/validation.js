8118f6304fe08923b37f101301e0806dfe861c8a
FHobbs8030 <FHobbs8030@Gmail.com>
2025-02-11 09:20:41 -0800

    change line 68 to modal__save-button_disabled

--- scripts/validation.js
+++ scripts/validation.js
@@ -65,7 +65,7 @@ const config = {
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
   saveButtonSelector: ".modal__save-button",
-  inactiveButtonClass: "modal__button_disabled",
+  inactiveButtonClass: "modal__save-button_disabled",
   inputErrorClass: "modal__input_type_error",
   errorClass: "modal__error_visible",
 };
