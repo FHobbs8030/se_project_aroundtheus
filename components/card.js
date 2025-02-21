f8091b0774d74256c3e1bf5f594ee4507a7523ca
FHobbs8030 <FHobbs8030@Gmail.com>
2025-02-21 06:44:48 -0800

    revise

--- components/card.js
+++ components/card.js
@@ -1,14 +1,13 @@
 export default class Card {
-  constructor(data, templateSelector, handleImageClick, openModal) {
+  constructor(data, templateSelector, handleImageClick) {
     this._data = data;
     this._template = document.querySelector(templateSelector).content;
     this._handleImageClick = handleImageClick;
-    // this._openModal = openModal;
   }
 
   _getTemplate() {
     const cardElement = this._template.cloneNode(true);
-    this._cardElement = cardElement; // Store the card element
+    this._cardElement = cardElement;
     this._cardImage = cardElement.querySelector(".cards__image");
     this._cardTitle = cardElement.querySelector(".cards__title");
     this._deleteButton = cardElement.querySelector(".cards__delete-button");
@@ -31,13 +30,15 @@ export default class Card {
 
     this._cardImage.addEventListener("click", () => {
       this._handleImageClick(this._data);
-      this._openModal(document.querySelector("#image-modal"));
     });
   }
 
   generateCard() {
-    this._getTemplate(); // Set class fields
-    this._setEventListeners(); // Set event listeners
+    this._getTemplate();
+    this._setEventListeners();
+    this._cardImage.src = this._data.link;
+    this._cardImage.alt = this._data.name;
+    this._cardTitle.textContent = this._data.name;
     return this._cardElement;
   }
 
