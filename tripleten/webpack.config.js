bed9363312b2d1efb92e685f32dc3d8fd0d4aa07
FHobbs8030 <FHobbs8030@Gmail.com>
2025-03-13 12:22:49 -0700

    modify

--- tripleten/webpack.config.js
+++ tripleten/webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development", 
   entry: "./index.js", 
   output: {
     filename: "bundle.js", 
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "dist"), 
   },
   module: {
     rules: [
