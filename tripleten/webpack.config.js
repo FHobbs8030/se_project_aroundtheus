904066bc5f86a72246a45b0c3256fc52f23d7c06
FHobbs8030 <FHobbs8030@Gmail.com>
2025-03-13 12:18:49 -0700

    modify

--- tripleten/webpack.config.js
+++ tripleten/webpack.config.js
@@ -1,34 +1,22 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./index.js", 
   output: {
-    filename: "bundle.js",
+    filename: "bundle.js", 
     path: path.resolve(__dirname, "dist"),
   },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
-      watch: true,
-    },
-    port: 8080,
-    open: true,
-  },
-
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
-      {
-        test: /\.(jpg|jpeg|png|svg)$/,
-        use: ["file-loader"],
-      },
     ],
   },
-  resolve: {
-    extensions: [".js", ".css"],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 8080,
   },
-  mode: "development",
 };
