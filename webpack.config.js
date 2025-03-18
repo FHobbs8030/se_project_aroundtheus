e587c07bcd74c187700037c11b59570f81bbcd1c
FHobbs8030 <FHobbs8030@Gmail.com>
2025-03-18 01:03:04 -0700

    new file, modify, rename

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "index.html",
+      favicon: "./src/images/favicon.ico", 
     }),
+  
   ],
 
   performance: {
