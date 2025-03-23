0033240800be4ed0e714b725d35745d1755dc782
FHobbs8030 <FHobbs8030@Gmail.com>
2025-03-23 01:51:02 -0700

    modify

--- src/components/Card.js
+++ src/components/Card.js
@@ -15,40 +15,53 @@ export default class Card {
 
   generateCard() {
     this._element = this._getTemplate();
-    const cardImage = this._element.querySelector(".card__image");
-    cardImage.src = this._data.link;
-    cardImage.alt = this._data.name;
-    this._element.querySelector(".card__title").textContent = this._data.name;
-    this._setEventListeners();
-    return this._element;
-  }
+    this._cardImage = this._element.querySelector(".card__image");
+    this._cardTitle = this._element.querySelector(".card__title");
+    this._likeButton = this._element.querySelector(".card__heart");
+    this._deleteButton = this._element.querySelector(".card__delete-button");
 
-  _handleLikeClick() {
-    const likeButton = this._element.querySelector(".card__heart");
-    likeButton.classList.toggle("card__heart_active");
+    this._cardImage.src = this._data.link;
+    this._cardImage.alt = this._data.name;
+    this._cardTitle.textContent = this._data.name;
+
+    this._setEventListeners();
+    this._toggleDeleteButtonVisibility();
+
+    return this._element;
   }
 
   _handleDeleteClick() {
     this._element.remove();
   }
 
+  _toggleDeleteButtonVisibility() {
+    if (this._isCardLiked()) {
+      this._deleteButton.classList.add("card__delete-button_hidden");
+    } else {
+      this._deleteButton.classList.remove("card__delete-button_hidden");
+    }
+  }
+
+  _handleLikeClick() {
+    this._likeButton.classList.toggle("card__heart_active");
+    this._toggleDeleteButtonVisibility();
+  }
+
+  _isCardLiked() {
+    return this._likeButton.classList.contains("card__heart_active");
+  }
+
   _setEventListeners() {
-    this._element
-      .querySelector(".card__image")
-      .addEventListener("click", () => {
-        this._handleCardClick(this._data);
-      });
+    this._cardImage.addEventListener("click", () => {
+      this._handleCardClick(this._data);
+    });
 
-    this._element
-      .querySelector(".card__heart")
-      .addEventListener("click", () => {
-        this._handleLikeClick();
-      });
+    this._likeButton.addEventListener("click", () => {
+      this._handleLikeClick();
+    });
 
-    this._element
-      .querySelector(".card__delete-button")
-      .addEventListener("click", () => {
-        this._handleDeleteClick();
-      });
+    this._deleteButton.addEventListener("click", () => {
+      this._handleDeleteClick();
+    });
   }
 }
